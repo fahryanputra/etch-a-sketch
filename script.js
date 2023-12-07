68a18e957045c08ba71681abfa6dfa067c44d837
fahryanputra <fahryanputra@gmail.com>
2023-12-07 13:17:08 +0700

    Allow user to select grid size

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const container = document.querySelector(".grid-container");
+const restartButton = document.querySelector(".restart");
 
-for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
-        const createGrid = document.createElement("div");
-        createGrid.setAttribute("class", "grid");
-        
-        container.appendChild(createGrid);
-    }
-};
+let gridSize = 16;
 
-const grids = document.querySelectorAll(".grid");
+function createGrid(gridSize) {
+    for(let i = 0; i < gridSize; i++) {
+        for(let j = 0; j < gridSize; j++) {
+            const gridSquare = document.createElement("div");
 
-Object.values(grids).forEach(grid => {
-    grid.addEventListener("mouseenter", (event) => {
-        event.target.setAttribute("style", "background-color: black");
-    });
-})
+            gridSquare.setAttribute("class", "grid");
+            gridSquare.setAttribute("style", `width: calc(100% * (1/${gridSize}))`)
+
+            container.appendChild(gridSquare);
+        };
+    };
+    let grids = document.querySelectorAll(".grid");
+
+    Object.values(grids).forEach(grid => {
+        grid.addEventListener("mouseenter", (event) => {
+            event.target.style.backgroundColor = "black";
+        });
+    })
+}
+
+restartButton.addEventListener("click", () => {
+    do {
+        gridSize = prompt("Enter grid size (max 100)!");
+    } while(gridSize > 100);
+
+   while(container.firstChild) {
+       container.removeChild(container.lastChild);
+   };
+
+   createGrid(gridSize);
+});
+
+createGrid(gridSize);
